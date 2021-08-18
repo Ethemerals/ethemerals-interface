c6a9c26ee2966c39fb198e27b3aed184a8d48203
tyrone <binktogether@gmail.com>
2021-08-18 02:26:18 -0700

    new contract

--- src/constants/contracts/Addresses.js
+++ src/constants/contracts/Addresses.js
@@ -1,6 +1,6 @@
 const Addresses = {
-	EthemeralLifeForce: '0xf3942ec9242661253f3b149f9e64c506980190c7',
-	Ethemerals: '0x66c0704b2c906c433ca80aa5f9bc8a92395c06da',
+	EthemeralLifeForce: '0xd3d064380f51c9b57242b1d5cacaf2f1187bd57b',
+	Ethemerals: '0xb0e7a9b4bb4bd06c853e2e74e759744494ab81e3',
 	EternalBattle: '0x163cb1c87b75fd5b12f6a77bb955dc6cc69cf1d0',
 	priceFeed: '0x1e704437f1323fda08358cedf5a3f9619fa11fc1',
 	admin: '0x67256ab8e993ed8647cbc4723883f4936058dff2',
