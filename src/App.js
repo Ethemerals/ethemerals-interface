3ae192e86e6382c6bd4584fac920a3bd34b5ef78
tyrone <binktogether@gmail.com>
2021-10-11 16:08:29 -0700

    added all

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ import Ethemerals from './pages/Ethemerals';
 import Equipable from './pages/Equipable';
 import NFTDetails from './pages/NFTDetails';
 import Opensea from './pages/Opensea';
+import All from './pages/All';
 // import Resurrect from './pages/Resurrect';
 // import Redemption from './pages/Redemption';
 // import Marketplace from './pages/Marketplace';
@@ -71,6 +72,9 @@ function App() {
 								</Route>
 								<Route exact path="/admin">
 									<Admin />
+								</Route>
+								<Route exact path="/all">
+									<All />
 								</Route>
 								{/* <Route exact path="/art">
 									<Art />
