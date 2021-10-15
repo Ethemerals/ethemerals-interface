2dcece0ef99275800453a0ad59f786563b8a9cc9
tyrone <binktogether@gmail.com>
2021-10-14 18:08:33 -0700

    remove consolelogs

--- src/hooks/useColorTraits.js
+++ src/hooks/useColorTraits.js
@@ -68,7 +68,6 @@ export const refreshMetadata = async (id) => {
 	try {
 		const response = await axios.get(`${baseUrl}${id}/?force_update=true`);
 		if (response.data) {
-			console.log(response.data);
 			return { success: true, data: response.data };
 		}
 	} catch (error) {
