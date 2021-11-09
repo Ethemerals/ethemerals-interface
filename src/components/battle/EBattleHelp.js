e0069dfed78322f3f55415d69e00a8789aeceb35
tyrone <binktogether@gmail.com>
2021-11-09 00:35:24 -0800

    help text

---
+++ src/components/battle/EBattleHelp.js
@@ -0,0 +1,20 @@
+const EBattleHelp = () => {
+	return (
+		<div style={{ maxWidth: '864px' }} className="bg-white bg-opacity-30 text-black text-left mx-auto mt-20 p-6">
+			<p>
+				Welcome to the first of many battle / gamified staking contract! <span className="text-xs font-bold text-brandColor">(Future development will be on Layer2)</span>
+			</p>
+			<h1 className="my-2 mt-8 text-brandColor-purple text-3xl">The Eternal Battle</h1>
+			<p>How To:</p>
+			<ul>
+				<li>- Choose a crypto market</li>
+				{/* <li>- Choose a crypto market</li> */}
+			</ul>
+			{/* <p>Pick an available market, </p> */}
+
+			<p className="text-red-500">Contract is functional, Help doc still in progress!</p>
+		</div>
+	);
+};
+
+export default EBattleHelp;
--- src/pages/EternalBattle.js
+++ src/pages/EternalBattle.js
@@ -2,6 +2,7 @@ import { useHistory, useParams } from 'react-router-dom';
 
 import PriceFeeds from '../constants/PriceFeeds';
 import EternalBattleCard from '../components/battle/EternalBattleCard';
+import EBattleHelp from '../components/battle/EBattleHelp';
 
 const EternalBattle = () => {
 	let { id } = useParams();
@@ -15,19 +16,20 @@ const EternalBattle = () => {
 	return (
 		<div className="scrollbar_pad">
 			<div className="page_bg"></div>
+			<EBattleHelp />
 
-			<div className="flex items-center mx-auto mt-2 sm:mt-10 text-sm sm:text-base justify-center">
-				<span className="text-xs font-bold px-2 text-white hidden sm:flex">MARKETS</span>
+			<div className="flex items-center mx-auto mt-2 sm:mt-20 text-sm sm:text-base justify-center">
+				<span className="text-xs font-bold px-2 text-blue-900">MARKETS</span>
 
 				<button
 					onClick={() => history.push(`/battle/0`)}
-					className={`${priceFeedId === 0 ? 'bg-indigo-500' : 'bg-indigo-300 hover:bg-yellow-400 transition duration-300'} py-1 px-2 mx-1 rounded focus:outline-none`}
+					className={`${priceFeedId === 0 ? 'bg-blue-500' : 'bg-blue-300 hover:bg-blue-400 transition duration-300'} py-1 px-2 mx-1 focus:outline-none`}
 				>
 					{PriceFeeds[0].ticker}
 				</button>
 				<button
 					onClick={() => history.push(`/battle/1`)}
-					className={`${priceFeedId === 1 ? 'bg-indigo-500' : 'bg-indigo-300 hover:bg-yellow-400 transition duration-300'} py-1 px-2 mx-1 rounded focus:outline-none`}
+					className={`${priceFeedId === 1 ? 'bg-blue-500' : 'bg-blue-300 hover:bg-blue-400 transition duration-300'} py-1 px-2 mx-1 focus:outline-none`}
 				>
 					{PriceFeeds[1].ticker}
 				</button>
